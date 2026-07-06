65807f43cebf4d05db88c684d62d5b67b30672b5
David Haney <david@stackslabs.com>
2026-07-06 14:30:14 -0400

    Revert "Remove unused import to make Cargo Hack / WASM Targets check happy"
    This reverts commit 48fda114ddae70fb053d5c98bdd3d8a96814dad7.

--- stacks-common/src/util/secp256k1/wasm.rs
+++ stacks-common/src/util/secp256k1/wasm.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use ::libsecp256k1::curve::Scalar;
 pub use ::libsecp256k1::Error;
 use ::libsecp256k1::{
     self, PublicKey as LibSecp256k1PublicKey, RecoveryId as LibSecp256k1RecoveryId,
